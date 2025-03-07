e1699e961346fa4395757afdfac4ef35b68ce4d5
bia <bsdeab@gmail.com>
2025-03-07 15:43:26 +0000

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,110 +1,166 @@
-import React, { useState, useEffect } from 'react';
-import './Projects.scss';
-import Filter from '../../components/Filter/Filter';
-import data from '../../data/infos.json';
-import translations from '../../data/translate.json';
+import React from 'react';
+import './Filter.scss';
+import Select from "react-select";
+import data from '../../data/infos.json'; // Importando o arquivo JSON
+import translations from '../../data/translate.json'; // Importando o JSON de tradução
 
-function Projects({ lang }) {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [selectedThemes, setSelectedThemes] = useState([]);
-    const [type, setType] = useState([]);
-    const [region, setRegion] = useState([]);
-    const [source, setSource] = useState([]);
-    const [languageFilter, setLanguageFilter] = useState([]);
-    const [organization, setOrganization] = useState([]);
-    const [paid, setPaid] = useState(null);
-    const [projects, setProjects] = useState([]);
-    const [filteredProjects, setFilteredProjects] = useState([]);
-
-    useEffect(() => {
-        setProjects(data[lang].Projects);
-        setFilteredProjects(data[lang].Projects);
-    }, [lang]);
-
-    useEffect(() => {
-        filterProjects();
-    }, [searchTerm, selectedThemes, type, region, source, languageFilter, organization, paid]);
-
-    const filterProjects = () => {
-        let filtered = projects;
-
-        if (searchTerm) {
-            filtered = filtered.filter((project) =>
-                project.name.toLowerCase().includes(searchTerm)
-            );
+function findEnglishKeyTheme(selectedValue) {
+    for (const [englishKey, translatedValues] of Object.entries(translations.Themes)) {
+        if (translatedValues.includes(selectedValue) || englishKey === selectedValue) {
+            return englishKey;
         }
+    }
+    return selectedValue; // Caso não encontre, mantém o valor original
+}
 
-        if (selectedThemes.length > 0) {
-            filtered = filtered.filter((project) =>
-                selectedThemes.every((theme) => project.themes.includes(theme))
-            );
+function findEnglishKeyType(selectedValue) {
+    for (const [englishKey, translatedValues] of Object.entries(translations.Types)) {
+        if (translatedValues.includes(selectedValue) || englishKey === selectedValue) {
+            return englishKey;
         }
+    }
+    return selectedValue; // Caso não encontre, mantém o valor original
+}
 
-        if (type.length > 0) {
-            filtered = filtered.filter((project) =>
-                type.every((t) => project.type.includes(t))
-            );
+function findEnglishKeyRegion(selectedValue) {
+    for (const [englishKey, translatedValues] of Object.entries(translations.Regions)) {
+        if (translatedValues.includes(selectedValue) || englishKey === selectedValue) {
+            return englishKey;
         }
+    }
+    return selectedValue; // Caso não encontre, mantém o valor original
+}
 
-        if (region.length > 0) {
-            filtered = filtered.filter((project) =>
-                region.every((r) => project.region.includes(r))
-            );
+function findEnglishKeySource(selectedValue) {
+    for (const [englishKey, translatedValues] of Object.entries(translations.Sources)) {
+        if (translatedValues.includes(selectedValue) || englishKey === selectedValue) {
+            return englishKey;
         }
+    }
+    return selectedValue; // Caso não encontre, mantém o valor original
+}
 
-        if (source.length > 0) {
-            filtered = filtered.filter((project) => {
-                console.log("Project Source:", project.source);
-                console.log("Selected Sources:", source);
-                const sourceMatch = source.includes(project.source);
-                console.log("Source Match:", sourceMatch);
-                return sourceMatch;
-            });
-        }
+function Filter({ setType, setRegion, setSource, setSelectedThemes, setSearchTerm, setLanguageFilter, setOrganization, setPaid, organizationsList, lang }) {
+    let language = lang;
 
-        if (languageFilter.length > 0) {
-            filtered = filtered.filter((project) =>
-                languageFilter.every((language) => project.languages.includes(language))
-            );
-        }
+    // Gerando as opções a partir do arquivo JSON
+    const themeOptions = Object.keys(data[language].Themes).map((themeKey) => ({
+        label: themeKey,
+        value: themeKey
+    }));
 
-        if (organization.length > 0) {
-            filtered = filtered.filter((project) =>
-                organization.every((org) => project.organization.includes(org))
-            );
-        }
+    const typeOptions = data[language].Types.map((type) => ({
+        label: type,
+        value: type
+    }));
 
-        if (paid !== null) {
-            filtered = filtered.filter((project) => project.paid === paid);
-        }
+    const regionOptions = data[language].Regions.map(region => ({
+        label: region,
+        value: region
+    }));
 
-        setFilteredProjects(filtered);
-    };
+    const sourceOptions = data[language].Sources.map(source => ({
+        label: source,
+        value: source
+    }));
+
+    // Opções de linguagem (exemplo: inglês, francês, etc.)
+    const languageOptions = [
+        { label: "English", value: "English" },
+        { label: "Français", value: "French" },
+        { label: "Español", value: "Spanish" },
+        { label: "Português", value: "Portuguese" },
+        { label: "नेपाली", value: "Nepali" },
+        { label: "हिंदी", value: "Hindi" },
+        { label: "Czhech", value: "Czech" },
+        { label: "Deutsch", value: "German" },
+        { label: "اللغة العربية", value: "Arabic" },
+        { label: "українська", value: "Ukrainian" }
+    ];
 
     return (
-        <div className='projects-page'>
-            <Filter
-                setType={setType}
-                setRegion={setRegion}
-                setSource={setSource}
-                setSelectedThemes={setSelectedThemes}
-                setSearchTerm={setSearchTerm}
-                setLanguageFilter={setLanguageFilter}
-                setOrganization={setOrganization}
-                setPaid={setPaid}
-                lang={lang}
-            />
-            <div className='projects-list'>
-                {filteredProjects.map((project) => (
-                    <div key={project.id} className='project-item'>
-                        <h3>{project.name}</h3>
-                        <p>Source: {project.source}</p>
-                        {/* Render other project details */}
-                    </div>
-                ))}
+        <div className='filter-container'>
+            <div className='filter-saves'>
+                <span className='filter-title'>{data[lang]["Texts"]["Projects"]["Filter"]["Name"]}</span>
+            </div>
+
+            <div className="filters">
+                <div className='filter-item'>
+                    <input
+                        id="search"
+                        className='search-input'
+                        placeholder={data[lang]["Texts"]["Projects"]["Filter"]["Search"]}
+                        onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
+                    />
+                </div>
+
+                <div className='filter-item'>
+                    <Select
+                        isMulti
+                        onChange={(selectedOptions) => {
+                            const selectedValues = selectedOptions ? selectedOptions.map(option => option.value) : [];
+                            const englishThemes = selectedValues.map(findEnglishKeyTheme);
+                            setSelectedThemes(englishThemes);
+                        }}
+                        options={themeOptions}
+                        placeholder={data[lang]["Texts"]["Projects"]["Filter"]["Themes"]}
+                    />
+                </div>
+
+                <div className='filter-item'>
+                    <Select
+                        isMulti
+                        onChange={(selectedOptions) => {
+                            const selectedValues = selectedOptions ? selectedOptions.map(option => option.value) : [];
+                            const englishType = selectedValues.map(findEnglishKeyType);
+                            setType(englishType);
+                        }}
+                        options={typeOptions}
+                        placeholder={data[lang]["Texts"]["Projects"]["Filter"]["Types"]}
+                    />
+                </div>
+
+                <div className='filter-item'>
+                    <Select
+                        isMulti
+                        onChange={(selectedOptions) => {
+                            const selectedValues = selectedOptions ? selectedOptions.map(option => option.value) : [];
+                            const englishRegion = selectedValues.map(findEnglishKeyRegion);
+                            setRegion(englishRegion);
+                        }}
+                        options={regionOptions}
+                        placeholder={data[lang]["Texts"]["Projects"]["Filter"]["Regions"]}
+                    />
+                </div>
+
+                <div className='filter-item'>
+                    <Select
+                        isMulti
+                        onChange={(selectedOptions) => {
+                            const selectedValues = selectedOptions ? selectedOptions.map(option => option.value) : [];
+                            const englishSource = selectedValues.map(findEnglishKeySource);
+                            setSource(englishSource);
+                        }}
+                        options={sourceOptions}
+                        placeholder={data[lang]["Texts"]["Projects"]["Filter"]["Sourcers"]}
+                    />
+                </div>
+
+                <div className='filter-item'>
+                    <Select
+                        isMulti
+                        onChange={(selectedOptions) => {
+                            const selectedValues = selectedOptions ? selectedOptions.map(option => option.value) : [];
+                            setLanguageFilter(selectedValues); // Atualiza o filtro de linguagem
+                        }}
+                        options={languageOptions}
+                        placeholder={data[lang]["Texts"]["Projects"]["Filter"]["Language"]}
+                    />
+                </div>
+
             </div>
         </div>
     );
 }
 
-export default Projects;
+export default Filter;
